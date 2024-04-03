e3990db8173374b0c9174e70abb17379013fe510
Josue Zorrilla <josuezorrilla2302@gmail.com>
2024-04-02 23:32:38 -0400

    Create the PhoneNumber value object

---
+++ Domain/ValueObjects/PhoneNumber.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects
+{
+    public partial record PhoneNumber
+    {
+        private const int DefaultLength = 9;
+        private const string Pattern = @"^(?:-*\d-*){8}$";
+        private PhoneNumber(string value) => value = value;
+
+        public static PhoneNumber? Create(string value)
+        {
+            if(string.IsNullOrEmpty(value) || !PhoneNumberRegex().IsMatch(value) || value.Length != DefaultLength)
+            {
+                return null;
+            }
+            return new PhoneNumber(value);
+        }
+
+        public string Value{ get; init;}
+
+        [GeneratedRegex(Pattern)]
+        private static partial Regex PhoneNumberRegex();
+    }
+}
